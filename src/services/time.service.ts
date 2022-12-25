615da9d12b50bb00db1a1e0867b00586ad80fb6c
Caio Giffoni <caio_cgf@hotmail.com>
2022-12-25 17:06:50 -0300

    :hammer: fix: string date

--- src/services/time.service.ts
+++ src/services/time.service.ts
@@ -4,7 +4,7 @@ export const TimeNowService = async () => {
 };
 
 export const TimeParService = async (date: string) => {
-  const now = date.includes("-") ? new Date(date) : new Date(Number(date));
+  const now = /^\d*$/.test(date) ? new Date(Number(date)) : new Date(date);
   if (!(now instanceof Date && !isNaN(Number(now))))
     return { error: "Invalid Date" };
   return { unix: now.getTime(), utc: now.toUTCString() };
